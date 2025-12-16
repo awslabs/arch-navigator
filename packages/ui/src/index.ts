f995a7947a7fa712173759fd0138ad84804d39a6
Luke Brassard <231255992+brassard-aws@users.noreply.github.com>
2025-12-16 12:04:55 -0500

    fix/dark-mode-icon: improving dark mode icon (#10)

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,6 @@
 import "./webawesome";
 
+export * from "./components/icons/index.ts";
 export * from "./components/Layout";
 export * from "./components/ResourceDetails";
 export * from "./components/ResourceTree";
